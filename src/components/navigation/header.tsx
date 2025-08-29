79e0c9435908e21f27d75cc3b28f401c1491e9f6
Shalva Gegia <shakogegia@users.noreply.github.com>
2025-08-29 17:46:37 +0200

    fix: nav buttons

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,11 +1,24 @@
+import { House, Search } from "lucide-react";
 import { SettingsDropdown } from "./settings-dropdown";
 import { ThemeSwitcher } from "./theme-switcher";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export function Header() {
   return (
     <header className="flex justify-between items-center p-4">
       <div />
       <div className="flex items-center gap-2">
+        <Link href="/">
+          <Button variant="outline" size="icon">
+            <House className="w-4 h-4" />
+          </Button>
+        </Link>
+        <Link href="/search">
+          <Button variant="outline" size="icon">
+            <Search className="w-4 h-4" />
+          </Button>
+        </Link>
         <ThemeSwitcher />
         <SettingsDropdown />
       </div>
