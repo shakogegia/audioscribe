103635c3566de8fc256dc2a1548d344008d2af63
Shalva Gegia <shalva.gegia@gmail.com>
2025-09-03 20:53:17 +0200

    fix: remove unused endpoints

--- src/app/api/book/[id]/bookmark/route.ts
+++ src/app/api/book/[id]/bookmark/route.ts
@@ -1,33 +1,17 @@
-import { NextRequest, NextResponse } from "next/server";
-import type * as Audiobookshelf from "@/types/audiobookshelf";
-import { deleteBookmark, updateBookmarks } from "@/lib/audiobookshelf";
+import { updateBookmarks } from "@/lib/audiobookshelf"
+import type * as Audiobookshelf from "@/types/audiobookshelf"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
-    const { id } = await params;
-    const body = (await request.json()) as { bookmarks: Audiobookshelf.AudioBookmark[] };
+    const { id } = await params
+    const body = (await request.json()) as { bookmarks: Audiobookshelf.AudioBookmark[] }
 
-    await updateBookmarks(id, body.bookmarks);
+    await updateBookmarks(id, body.bookmarks)
 
-    return NextResponse.json({ message: "Bookmark updated" }, { status: 200 });
+    return NextResponse.json({ message: "Bookmark updated" }, { status: 200 })
   } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
-  }
-}
-
-export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
-  try {
-    const { id } = await params;
-    const { searchParams } = new URL(request.url);
-
-    const time = Number(searchParams.get("time"));
-
-    await deleteBookmark(id, Number(time));
-
-    return NextResponse.json({ message: "Bookmark deleted" }, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    console.error(error)
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 })
   }
 }
