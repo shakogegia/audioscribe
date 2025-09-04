2e53baebeb8442f71ba88c0108e0b233a461a740
Shalva Gegia <shalva.gegia@gmail.com>
2025-09-04 18:53:36 +0200

    fix: prompt file

--- src/ai/helpers/prompt.ts
+++ src/ai/helpers/prompt.ts
@@ -1,11 +1,11 @@
-import fs from "fs/promises";
-import handlebars from "handlebars";
-import path from "path";
+import fs from "fs/promises"
+import handlebars from "handlebars"
+import path from "path"
 
-type TemplateName = "bookmark-suggestions" | "book-query";
+type TemplateName = "bookmark-suggestions" | "book-query"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function createPrompt(template: TemplateName, params: any): Promise<string> {
-  const prompt = await fs.readFile(path.join(process.cwd(), "src/prompts", `${template}.md`), "utf8");
-  return handlebars.compile(prompt)(params);
+  const prompt = await fs.readFile(path.join(process.cwd(), "src/prompts", `${template}.txt`), "utf8")
+  return handlebars.compile(prompt)(params)
 }
