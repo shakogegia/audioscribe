74263b062ced7b7cb2284d1e930bee2c21d4f2ad
Shalva Gegia <shalva.gegia@gmail.com>
2025-09-09 23:53:59 +0200

    fix: dark theme logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,19 +3,17 @@ import { useTheme } from "next-themes"
 import Image from "next/image"
 
 export default function Logo({ size = 128 }: { size?: number }) {
-  const { systemTheme } = useTheme()
-  const src = systemTheme === "dark" ? "/logo/logo-light.png" : "/logo/logo-dark.png"
+  const { resolvedTheme } = useTheme()
+  const src = resolvedTheme === "dark" ? "/logo/logo-mono-dark.png" : "/logo/logo-dark.png"
   return (
     <Image
       suppressHydrationWarning
-      src={src}
+      src={`${src}?v=${Date.now()}`}
       alt="AudioScribe logo"
       width={size}
       height={size}
       priority
       loading="eager"
-      placeholder="blur"
-      blurDataURL={src}
       unoptimized
     />
   )
