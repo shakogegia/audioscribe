01e2a5ea57c629b60f5c25f5c8cf60f91ca17c16
Shalva Gegia <shalva.gegia@gmail.com>
2025-12-14 12:50:16 +0100

    fix: manifest

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,5 +47,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|.*\\.png$|.*\\.ico$|.*\\.svg$|site\\.webmanifest).*)"],
+  matcher: ["/((?!_next/static|_next/image|.*\\.png$|.*\\.ico$|.*\\.svg$|.*\\.webmanifest$).*)"],
 }
