97a181cab4e5120714bb3760418c1486f1dd6810
Shalva Gegia <shalva.gegia@gmail.com>
2025-12-15 21:05:06 +0100

    fix: revalidate

--- src/app/(authenticated)/(navigation)/(main)/book/[id]/page.tsx
+++ src/app/(authenticated)/(navigation)/(main)/book/[id]/page.tsx
@@ -7,6 +7,9 @@ async function revalidateBook(id: string) {
   revalidatePath(`/book/${id}`)
 }
 
+// do not cache this page
+export const dynamic = "force-dynamic"
+
 export default async function BookPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
 
