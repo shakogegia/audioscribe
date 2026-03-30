166c33135f1c2289f2096f2edaee9e53c22b43e6
Shalva Gegia <shalva.gegia@gmail.com>
2026-03-30 22:25:19 +0200

    fix(shortcuts): adjust grid layout for shortcut cards
    Updated the grid layout in the ShortcutsPage component to improve responsiveness, changing the column configuration to 2 columns on smaller screens and 4 columns on larger screens.

--- src/app/(authenticated)/(navigation)/(ios)/ios/shortcuts/page.tsx
+++ src/app/(authenticated)/(navigation)/(ios)/ios/shortcuts/page.tsx
@@ -12,7 +12,7 @@ export default async function ShortcutsPage() {
         </p>
       </div>
 
-      <div className="grid grid-cols-4 gap-8">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
         <ShortcutCard
           title="Previously on"
           icon={CirclePlayIcon}
